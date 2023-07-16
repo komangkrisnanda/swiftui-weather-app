bafc560a860e1946b79165401c0f10d56fe49071
Komang Krisnanda <officialkrisnandha@gmail.com>
2023-07-16 22:29:06 +0800

    Initial Commit

---
+++ Shared/todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolistApp.swift
+//  Shared
+//
+//  Created by Komang Krisnanda on 16/07/23.
+//
+
+import SwiftUI
+
+@main
+struct todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
